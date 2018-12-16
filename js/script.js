a95ce761a1ea12f5e9436e101f761774a89b7c9c
Alexandr Kolosovsky <kolo-ky@yandex.ru>
2018-12-16 16:17:34 +0500

    добавляет интерактивность модальным окнам

--- js/script.js
+++ js/script.js
@@ -1,55 +1,200 @@
-/* Скрипт появления/скрытия попапа */
+/* Скрипт появления/скрытия попапа формы обратной связи */
 
-var popup = function (payload) {
-  var currentPopup = document.querySelector(payload.selector).closest('.popup');
-  var closePopupBtn = currentPopup.querySelector('.popup__button');
+(function () {
+  var openFeedbackBtn = document.querySelector('.show-popup-form');
 
-  closePopupBtn.addEventListener('click', close);
+  if (!openFeedbackBtn) {
+    return;
+  }
 
-  function show (evt) {
+  var feedbackModal = document.querySelector('.popup');
+  var popupContainer = feedbackModal.querySelector('.popup__container');
+  var closeFeedbackBtn = feedbackModal.querySelector('.popup__button');
+
+  var form = feedbackModal.querySelector('.feedback-form__form');
+  var userName = form.querySelector('input[name="user-name"]');
+  var mail = form.querySelector('input[name="mail"]');
+  var text = form.querySelector('textarea[name="text"]');
+
+  function showFeedbackModal (evt) {
     evt.preventDefault();
 
-    currentPopup.classList.add('popup-show');
+    feedbackModal.classList.add('popup--show');
+    popupContainer.classList.add('popup__container--show');
+    popupContainer.classList.remove('popup__container--error');
+    userName.focus();
   }
 
-  function close (evt) {
+  function closeFeedbackModal (evt) {
     evt.preventDefault();
 
-    currentPopup.classList.remove('popup-show');
+    feedbackModal.classList.remove('popup--show');
+    popupContainer.classList.remove('popup__container--show');
+    popupContainer.classList.remove('popup__container--error');
   }
 
-  return {
-    show: show
-  };
-};
+  function submitForm (evt) {
 
-/* Скрипт появления формы */
 
-var showFeedback = function () {
-  var feedbackShowButton = document.querySelector('.show-popup-form');
+    if (!userName.value || !mail.value || !text.value) {
+      evt.preventDefault();
 
-  if (feedbackShowButton) {
-    var feedbackPopup = popup({
-      selector: '.feedback-form'
-    });
-    feedbackShowButton.addEventListener('click', feedbackPopup.show);
+      popupContainer.classList.add('popup__container--error');
+    } else {
+      popupContainer.classList.remove('popup__container--error');
+      closeFeedbackModal(evt);
+    }
   }
-};
 
-/* Скрипт появления текста */
+  window.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
 
-var showText = function () {
-  var textShowButton = document.querySelector('.show-popup-text');
+      if (feedbackModal.classList.contains("popup--show")) {
+        evt.preventDefault();
 
-  if (textShowButton) {
-    var textPopup = popup({
-      selector: '.test-text'
-    });
-    textShowButton.addEventListener('click', textPopup.show);
+        popupContainer.classList.remove('popup__container--error');
+        feedbackModal.classList.remove("popup--show");
+      }
+    }
+  });
+
+  openFeedbackBtn.addEventListener('click', showFeedbackModal);
+  closeFeedbackBtn.addEventListener('click', closeFeedbackModal);
+  form.addEventListener('submit', submitForm);
+})();
+
+/* Скрипт открытия поля поиска */
+
+(function () {
+  var searchBtn = document.querySelector('.search__btn');
+  var searchModal = document.querySelector('.search__modal');
+
+  var form = searchModal.querySelector('.search__from');
+  var searchField = form.querySelector('input[name="search-field"]');
+
+  function showSearch(evt) {
+    evt.preventDefault();
+
+    searchBtn.classList.add('search__btn--active');
+    searchModal.classList.add('search-modal--show');
+    searchModal.classList.remove('search-modal--error');
+    searchField.focus();
   }
-};
 
-/* Вызов */
+  function submitForm (evt) {
+    if (!searchField.value) {
+      evt.preventDefault();
 
-showFeedback();
-showText();
+      searchModal.classList.add('search-modal--error');
+    } else {
+      evt.preventDefault();
+
+      searchModal.classList.remove('search-modal--show');
+      searchModal.classList.remove('search-modal--error');
+      searchBtn.classList.remove('search__btn--active');
+    }
+  }
+
+  window.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+
+      if (searchModal.classList.contains('search-modal--show')) {
+        searchModal.classList.remove('search-modal--show');
+        searchModal.classList.remove('search-modal--error');
+        searchBtn.classList.remove('search__btn--active');
+      }
+    }
+  });
+
+  searchBtn.addEventListener('click', showSearch);
+  form.addEventListener('submit', submitForm);
+})();
+
+/* Скрипт открытия формы входа */
+
+(function () {
+  var logInBtn = document.querySelector('.user__menu-link');
+  var userModal = document.querySelector('.user__modal');
+
+  var form = userModal.querySelector('.user__form');
+  var password = form.querySelector('input[name="password"]');
+  var mail = form.querySelector('input[name="login-mail"]');
+
+  function showUserModal(evt) {
+    evt.preventDefault();
+
+    logInBtn.classList.add('user__menu-link--active');
+    userModal.classList.add('user__modal--show');
+    userModal.classList.remove('user__modal--error');
+    mail.focus();
+  }
+
+  function submitForm (evt) {
+    if (!password.value || !mail.value) {
+      evt.preventDefault();
+
+      userModal.classList.add('user__modal--error');
+    } else {
+      evt.preventDefault();
+
+      userModal.classList.remove('user__modal--show');
+      userModal.classList.remove('user__modal--error');
+      logInBtn.classList.remove('user__menu-link--active');
+    }
+  }
+
+  window.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+
+      if (userModal.classList.contains('user__modal--show')) {
+        evt.preventDefault();
+
+        userModal.classList.remove('user__modal--show');
+        userModal.classList.remove('user__modal--error');
+        logInBtn.classList.remove('user__menu-link--active');
+      }
+    }
+  });
+
+  logInBtn.addEventListener('click', showUserModal);
+  form.addEventListener('submit', submitForm);
+})();
+
+/* Скрипт открытия корзины */
+
+(function () {
+  var cartFill = document.querySelector('.cart--fill');
+
+  if (!cartFill) {
+    return;
+  }
+
+  var cartBtn = document.querySelector('.cart__btn');
+  var cartModal = cartFill.querySelector('.cart__modal');
+
+  function showCartModal(evt) {
+    evt.preventDefault();
+
+    cartBtn.classList.add('cart__btn--active');
+    cartModal.classList.add('cart-modal--show');
+  }
+
+  window.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+
+      if (cartModal.classList.contains('cart-modal--show')) {
+        evt.preventDefault();
+
+        cartModal.classList.remove('cart-modal--show');
+        cartBtn.classList.remove('cart__btn--active');
+      }
+    }
+  });
+
+  cartBtn.addEventListener('click', showCartModal);
+})();
+
